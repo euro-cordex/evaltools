0b0e1d4fef2dac7c0b566ffd37a99ed1da9e4159
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-11-20 17:30:27 +0100

    Update obs.py

--- evaltools/obs.py
+++ evaltools/obs.py
@@ -2,7 +2,11 @@ import xarray as xr
 
 
 def eobs():
-    """open EOBS dataset from LEAP"""
+    """open EOBS dataset from LEAP
+    
+    See also: https://catalog.leap.columbia.edu/feedstock/eobs-dataset
+    
+    """
     store = "https://ncsa.osn.xsede.org/Pangeo/pangeo-forge/pangeo-forge/EOBS-feedstock/eobs-tg-tn-tx-rr-hu-pp.zarr"
 
     return xr.open_dataset(store, engine="zarr", chunks={})
