a4474dd8654b9884d864d97ae420c573886e0715
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-11-22 20:49:35 +0000

    added era5

--- evaltools/obs.py
+++ evaltools/obs.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import numpy as np
 
 
 def eobs():
@@ -9,4 +10,25 @@ def eobs():
     """
     store = "https://ncsa.osn.xsede.org/Pangeo/pangeo-forge/pangeo-forge/EOBS-feedstock/eobs-tg-tn-tx-rr-hu-pp.zarr"
 
-    return xr.open_dataset(store, engine="zarr", chunks={})
+    ds = xr.open_dataset(store, engine="zarr", chunks={})
+
+    mask_var = "tg"
+
+    if add_mask is True:
+        # create a simple mask from missing values
+        ds["mask"] = xr.where(~np.isnan(ds[mask_var].isel(time=0)), 1, 0)
+
+    return ds
+
+
+def era5():
+    store = (
+        "gs://gcp-public-data-arco-era5/ar/1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2"
+    )
+    ds = xr.open_zarr(
+        store,
+        chunks={"time": 48},
+        consolidated=True,
+    )
+    ds["mask"] = ds.land_sea_mask
+    return ds
