01f05895c00d2969823d9b0dcda5678fa020c3a8
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-12-05 10:56:33 +0000

    added metrics.py

---
+++ evaltools/metrics.py
@@ -0,0 +1,44 @@
+#
+# from Kotlarski, S., Keuler, K., Christensen, O. B., Colette, A., Déqué, M., Gobiet, A., Goergen, K., Jacob,
+# D., Lüthi, D., van Meijgaard, E., Nikulin, G., Schär, C., Teichmann, C., Vautard, R., Warrach-Sagi, K.,
+# and Wulfmeyer, V.: Regional climate modeling on European scales: a joint standard evaluation of the
+# EURO-CORDEX RCM ensemble, Geosci. Model Dev., 7, 1297–1333, https://doi.org/10.5194/gmd-7-1297-2014, 2014.
+
+# BIAS: the difference (model − reference) of spatially
+# averaged climatological annual or seasonal mean values
+# for a selected subregion (relative difference for precipi-
+# tation).
+
+# 95 %-P: the 95th percentile of all absolute grid cell dif-
+# ferences (model − reference) across a selected subre-
+# gion based on climatological annual or seasonal mean
+# values (relative difference for precipitation).
+
+# PACO: the spatial pattern correlation between climato-
+# logical annual or seasonal mean values of model and
+# reference data across all grid points of a selected subre-
+# gion.
+
+# RSV: ratio (model over reference) of spatial standard de-
+# viations across all grid points of a selected subregion of
+# climatological annual or seasonal mean values.
+
+# TCOIAV: temporal correlation of interannual variabil-
+# ity between model and reference time series of spatially
+# averaged annual or seasonal mean values of a selected
+# subregion.
+
+# RIAV: ratio (model over reference) of temporal standard
+# deviations of interannual time series of spatially aver-
+# aged annual or seasonal mean values of a selected sub-
+# region.
+
+# CRCO: Spearman rank correlation between spatially av-
+# eraged monthly values of model and reference data of
+# the climatological mean annual cycle of a selected sub-
+# region.
+
+# ROYA: ratio (model over reference) of yearly amplitudes
+# (differences between maximum and minimum) of spa-
+# tially averaged monthly values of the climatological
+# mean annual cycle of a selected subregion.
