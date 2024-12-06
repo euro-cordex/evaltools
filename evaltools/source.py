53101546676d9b83be3996d9d559e6d4db175f29
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-12-06 13:12:55 +0000

    added merge and concat

--- evaltools/source.py
+++ evaltools/source.py
@@ -1,7 +1,8 @@
 import xarray as xr
 import intake
 
-xarray_open_kwargs = {"use_cftime": True, "decode_coords": "all"}
+xarray_open_kwargs = {"use_cftime": True, "decode_coords": "all", "chunks": None}
+time_range_default = slice("1979", "2020")
 
 
 def open_catalog(url=None):
@@ -19,7 +20,7 @@ def open_catalog(url=None):
     return intake.open_esm_datastore(url)
 
 
-def get_source_collection(variable_id, frequency, catalog=None):
+def get_source_collection(variable_id, frequency, fx=False, catalog=None):
     """
     Search the catalog for datasets matching the specified variable_id and frequency.
 
@@ -40,7 +41,7 @@ def get_source_collection(variable_id, frequency, catalog=None):
     return cat
 
 
-def open_and_sort(catalog, merge=False):
+def open_and_sort(catalog, merge=False, concat=False, time_range="auto"):
     """
     Convert the catalog to a dictionary of xarray datasets, sort them by source_id, and optionally merge the datasets.
 
@@ -51,6 +52,10 @@ def open_and_sort(catalog, merge=False):
     Returns:
     dict: A dictionary of sorted (and optionally merged) xarray datasets.
     """
+    if time_range == "auto":
+        time_range = time_range_default
+    if concat is True:
+        merge = True
     source_ids = list(catalog.df.source_id.unique())
     dsets = catalog.to_dataset_dict(xarray_open_kwargs=xarray_open_kwargs)
     print(f"Found {len(dsets)} datasets")
@@ -59,8 +64,24 @@ def open_and_sort(catalog, merge=False):
         sorted[source_id] = [
             dsets[key] for key in dsets if dsets[key].attrs["source_id"] == source_id
         ]
+    if time_range is not None:
+        for source_id in source_ids:
+            print(f"subsetting: {source_id}")
+            for i, ds in enumerate(sorted[source_id]):
+                if "time" in ds.dims:
+                    sorted[source_id][i] = ds.sel(time=time_range)
     if merge is True:
         for source_id in source_ids:
             print(f"merging: {source_id}")
             sorted[source_id] = xr.merge(sorted[source_id])
+    if concat is True:
+        ids = list(sorted.keys())
+        concat_dim = xr.DataArray(ids, dims="source_id", name="source_id")
+        return xr.concat(
+            list(sorted.values()),
+            dim=concat_dim,
+            compat="override",
+            coords="minimal",
+            join="override",
+        )
     return sorted
