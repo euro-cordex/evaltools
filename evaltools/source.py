c6368d02babe4e3ec8e27e0e05818bd474473050
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2024-12-06 12:18:30 +0000

    udpate docstring

--- evaltools/source.py
+++ evaltools/source.py
@@ -1,17 +1,36 @@
 import xarray as xr
 import intake
 
-
 xarray_open_kwargs = {"use_cftime": True, "decode_coords": "all"}
 
 
 def open_catalog(url=None):
+    """
+    Open a data catalog from a given URL. If no URL is provided, use the default URL.
+
+    Parameters:
+    url (str, optional): The URL of the data catalog. Defaults to None.
+
+    Returns:
+    intake.catalog: The opened data catalog.
+    """
     if url is None:
         url = "https://raw.githubusercontent.com/euro-cordex/joint-evaluation/refs/heads/main/CORDEX-CMIP6.json"
     return intake.open_esm_datastore(url)
 
 
 def get_source_collection(variable_id, frequency, catalog=None):
+    """
+    Search the catalog for datasets matching the specified variable_id and frequency.
+
+    Parameters:
+    variable_id (str): The variable ID to search for.
+    frequency (str): The frequency to search for.
+    catalog (intake.catalog, optional): The data catalog to search in. If None, open the default catalog.
+
+    Returns:
+    intake.catalog: The filtered data catalog.
+    """
     if catalog is None:
         catalog = open_catalog()
     cat = catalog.search(
@@ -22,6 +41,16 @@ def get_source_collection(variable_id, frequency, catalog=None):
 
 
 def open_and_sort(catalog, merge=False):
+    """
+    Convert the catalog to a dictionary of xarray datasets, sort them by source_id, and optionally merge the datasets.
+
+    Parameters:
+    catalog (intake.catalog): The data catalog to convert and sort.
+    merge (bool): Whether to merge the datasets for each source_id. Defaults to False.
+
+    Returns:
+    dict: A dictionary of sorted (and optionally merged) xarray datasets.
+    """
     source_ids = list(catalog.df.source_id.unique())
     dsets = catalog.to_dataset_dict(xarray_open_kwargs=xarray_open_kwargs)
     print(f"Found {len(dsets)} datasets")
